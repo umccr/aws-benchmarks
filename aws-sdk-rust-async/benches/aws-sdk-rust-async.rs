557a72a8786b1113424d4494a48d264c0715ce65
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-06-30 21:00:58 +1000

    According to https://medium.com/@softprops/configuration-envy-a09584386705: 'Envy assumes the snake_cased names of your struct map to SCREAMING_SNAKE_CASE env variable names provided by your process env'

--- aws-sdk-rust-async/benches/aws-sdk-rust-async.rs
+++ aws-sdk-rust-async/benches/aws-sdk-rust-async.rs
@@ -6,13 +6,12 @@ use aws_sdk_bench_async::upload::do_upload;
 
 use serde::Deserialize;
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 struct Config {
-    AWS_BENCH_OBJECT_NUM: usize,
-    AWS_BUCKET: String,
-    AWS_KEY: String,
-    AWS_REGION: String,
+    aws_bench_object_num: usize,
+    aws_bucket: String,
+    aws_key: String,
+    aws_region: String,
 }
 
 fn transfers(c: &mut Criterion)  {
@@ -22,15 +21,15 @@ fn transfers(c: &mut Criterion)  {
     for obj_size in [2 * MB, 8 * MB, 16 * MB, 32 * MB, 64 * MB].iter() {
         group.throughput(Throughput::Bytes(*obj_size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(obj_size), obj_size, |b, &obj_size| {
-            b.iter(|| do_upload(black_box(config.AWS_BENCH_OBJECT_NUM),
+            b.iter(|| do_upload(black_box(config.aws_bench_object_num),
                                    black_box(obj_size),
-                                black_box(config.AWS_BUCKET.clone()),
-                                   black_box(config.AWS_KEY.clone()),
-                                   black_box(config.AWS_REGION.clone())));
+                                black_box(config.aws_bucket.clone()),
+                                   black_box(config.aws_bucket.clone()),
+                                   black_box(config.aws_region.clone())));
 
-            b.iter(|| do_download(black_box(config.AWS_BUCKET.clone()),
-                                   black_box(config.AWS_KEY.clone()),
-                                   black_box(config.AWS_REGION.clone())));
+            b.iter(|| do_download(black_box(config.aws_bucket.clone()),
+                                   black_box(config.aws_key.clone()),
+                                   black_box(config.aws_region.clone())));
         });
 
     }
