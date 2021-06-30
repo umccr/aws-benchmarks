59d522702d6a4c61c61ae20e4a47236090e82005
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-06-30 16:17:12 +1000

    Try out envy for the first time and failing at it for now, :nudges: @softprops ;)

--- aws-sdk-rust-async/benches/aws-sdk-rust-async.rs
+++ aws-sdk-rust-async/benches/aws-sdk-rust-async.rs
@@ -4,41 +4,33 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughpu
 use aws_sdk_bench_async::download::do_download;
 use aws_sdk_bench_async::upload::do_upload;
 
-use structopt::StructOpt;
+use serde::Deserialize;
 
-#[derive(StructOpt, Debug)]
-struct Args {
-    #[structopt(long = "obj_number", default_value = "10")]
-    obj_number: usize,
-    #[structopt(long = "obj_size", default_value = "1024")]
-    bucket: String,
-    #[structopt(long = "key", default_value = "test-object-8388608", env)]
-    key: String,
-    #[structopt(long = "aws_region", default_value = "ap-southeast-2", env)]
-    region: String,
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+struct Config {
+    AWS_BENCH_OBJECT_NUM: usize,
+    AWS_BUCKET: String,
+    AWS_KEY: String,
+    AWS_REGION: String,
 }
 
-fn transfers(c: &mut Criterion) {
-    let Args {
-        obj_number,
-        bucket,
-        key,
-        region,
-    } = Args::from_args();
+fn transfers(c: &mut Criterion)  {
+    let config = envy::from_env::<Config>().expect("Something went wrong!");
 
     let mut group = c.benchmark_group("transfers");
     for obj_size in [2 * MB, 8 * MB, 16 * MB, 32 * MB, 64 * MB].iter() {
         group.throughput(Throughput::Bytes(*obj_size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(obj_size), obj_size, |b, &obj_size| {
-            b.iter(|| do_upload(black_box(obj_number),
+            b.iter(|| do_upload(black_box(config.AWS_BENCH_OBJECT_NUM),
                                    black_box(obj_size),
-                                black_box(bucket.clone()),
-                                   black_box(key.clone()),
-                                   black_box(region.clone())));
+                                black_box(config.AWS_BUCKET.clone()),
+                                   black_box(config.AWS_KEY.clone()),
+                                   black_box(config.AWS_REGION.clone())));
 
-            b.iter(|| do_download(black_box(bucket.clone()),
-                                   black_box(key.clone()),
-                                   black_box(region.clone())));
+            b.iter(|| do_download(black_box(config.AWS_BUCKET.clone()),
+                                   black_box(config.AWS_KEY.clone()),
+                                   black_box(config.AWS_REGION.clone())));
         });
 
     }
